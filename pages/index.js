bd897d1993179bf8813cd56bdac20b977b5742ba
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-07-23 09:27:32 -0500

    Add switches

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import AddIcon from "@material-ui/icons/Add";
+import Switch from "@material-ui/core/Switch";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const useStyles = makeStyles(theme => ({}));
 
 export default function ProjectManager() {
   const classes = useStyles();
   const theme = useTheme();
+
+  const [websiteChecked, setWebsiteChecked] = useState(false);
+  const [iOSChecked, setiOSChecked] = useState(false);
+  const [androidChecked, setAndroidChecked] = useState(false);
+  const [softwareChecked, setSoftwareChecked] = useState(false);
 
   return (
     <Grid container direction="column">
@@ -29,6 +37,57 @@ export default function ProjectManager() {
             ),
           }}
         />
+      </Grid>
+      <Grid item style={{ marginLeft: "5em", marginTop: "2em" }}>
+        <FormGroup row>
+          <FormControlLabel
+            style={{ marginRight: "5em" }}
+            control={
+              <Switch
+                checked={websiteChecked}
+                color="primary"
+                onChange={() => setWebsiteChecked(!websiteChecked)}
+              />
+            }
+            label="Websites"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            style={{ marginRight: "5em" }}
+            control={
+              <Switch
+                checked={iOSChecked}
+                color="primary"
+                onChange={() => setiOSChecked(!iOSChecked)}
+              />
+            }
+            label="iOS Apps"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            style={{ marginRight: "5em" }}
+            control={
+              <Switch
+                checked={androidChecked}
+                color="primary"
+                onChange={() => setAndroidChecked(!androidChecked)}
+              />
+            }
+            label="Android Apps"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={softwareChecked}
+                color="primary"
+                onChange={() => setSoftwareChecked(!softwareChecked)}
+              />
+            }
+            label="Custom Software"
+            labelPlacement="start"
+          />
+        </FormGroup>
       </Grid>
     </Grid>
   );
