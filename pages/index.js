77720fc90425c88915422fcb49bf8057881ec70a
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-07-23 09:11:33 -0500

    Text field for search and add

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import AddIcon from "@material-ui/icons/Add";
 
 const useStyles = makeStyles(theme => ({}));
 
@@ -11,8 +14,21 @@ export default function ProjectManager() {
 
   return (
     <Grid container direction="column">
-      <Grid item>
+      <Grid item style={{ marginTop: "2em", marginLeft: "5em" }}>
         <Typography variant="h1">Projects</Typography>
+      </Grid>
+      <Grid item>
+        <TextField
+          placeholder="Search project details or create a new entry."
+          style={{ width: "35em", marginLeft: "5em" }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <AddIcon color="primary" />
+              </InputAdornment>
+            ),
+          }}
+        />
       </Grid>
     </Grid>
   );
